2aedd7238c587af12f824f0f85ef9946dae0216c
Kasun <kperumbuli91@gmail.com>
2025-07-21 01:35:46 +0100

    mongdb link reading set to ead from os.getenv

--- customerchurn/components/data_ingestion.py
+++ customerchurn/components/data_ingestion.py
@@ -21,6 +21,10 @@ from dotenv import load_dotenv # read from .env file.
 load_dotenv()
 
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+if not MONGO_DB_URL:
+    raise ValueError("URL not set")
+
+
 
 class DataIngestion:
     '''
@@ -51,7 +55,7 @@ class DataIngestion:
         try:
             database_name = self.data_ingestion_config.database_name
             collection_name = self.data_ingestion_config.collection_name
-            self.mongo_client = pymongo.MongoClient(os.getenv("MONGO_DB_URL"))
+            self.mongo_client = pymongo.MongoClient(os.getenv(MONGO_DB_URL))
             collection = self.mongo_client[database_name][collection_name]
 
             df = pd.DataFrame(list(collection.find()))
